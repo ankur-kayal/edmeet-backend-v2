49790779abeb96d70dcf5ffc4d3f0212fc1f85de
Ankur Kayal <ankuraug2000@gmail.com>
2022-06-28 15:18:28 +0530

    Added updateUser and removeUser

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,8 +1,34 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
 import { UserService } from './user.service';
 import { User } from './entities/user.entity';
+import { Logger, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { UpdateUserInput } from './dto/update-user.input';
 
 @Resolver(() => User)
 export class UserResolver {
-  constructor(private readonly userService: UserService) {}
+  logger: Logger;
+  constructor(private readonly userService: UserService) {
+    this.logger = new Logger(UserResolver.name);
+  }
+
+  @Mutation(() => User)
+  @UseGuards(JwtAuthGuard)
+  async updateUser(
+    @Args('updateUserInput') updateUserInput: UpdateUserInput,
+    @Context() context,
+  ) {
+    this.logger.log(context.req.user);
+    return await this.userService.update(
+      context.req.user.userId,
+      updateUserInput,
+    );
+  }
+
+  @Mutation(() => String)
+  @UseGuards(JwtAuthGuard)
+  async removeUser(@Context() context) {
+    this.logger.log('Removing User: ', context.req.user);
+    return await this.userService.remove(context.req.user.userId);
+  }
 }
